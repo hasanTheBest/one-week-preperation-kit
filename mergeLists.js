d48ac4058a550f04c82e21b0c6402dc0e6130e2c
Mahmud <rainbowmahmud@gmail.com>
2022-02-11 23:17:53 +0600

    mergeList passed only 2 of 7 test cases.

--- mergeLists.js
+++ mergeLists.js
@@ -1,75 +1,47 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-
 function mergeLists(head1, head2) {
-  if (head1 === null) return head2;
-  if (head2 === null) return head1;
+  // if (head1 === null) return head2;
+  // if (head2 === null) return head1;
 
-  let mergedList = null;
-  // both list have data
-  // let tempH1 = head1, tempH2 = head2;
+  let mergedList = null,
+    nodesArr = [];
 
   while (head1 && head2) {
-    let h1 = head1.data;
-    let h2 = head2.data;
-
-    console.log("h1", h1);
-    console.log("h2", h2);
-
-    const h1Node = new Node(h1);
-    const h2Node = new Node(h2);
-
-    if (mergedList === null) {
-      if (h1 < h2) {
-        mergedList = h1Node;
-        mergedList.next = h2Node;
-      } else {
-        mergedList = h2Node;
-        mergedList.next = h1Node;
-      }
-    }
-
-    // if (h1 > h2) {
-    //   let node = new Node(h2);
-    //   if (mergedList === null) {
-    //     mergedList = node;
-    //   } else {
-    //     tempList = mergedList;
-    //     while (tempList.next != null) {
-    //       tempList = tempList.next;
-    //     }
-
-    //     mergedList = tempList;
-    //   }
-    // } else {
-    //   let node = new Node(h1);
-
-    //   if (mergedList === null) {
-    //     mergedList = node;
-    //   } else {
-    //     tempList = mergedList;
-    //     while (tempList.next != null) {
-    //       tempList = tempList.next;
-    //     }
-
-    //     mergedList = node;
-    //   }
-    // }
+    nodesArr.push(head1.data);
+    nodesArr.push(head2.data);
 
     // traverse one level dipper
     head1 = head1.next;
     head2 = head2.next;
-
-    console.log("head1", head1);
-    console.log("head2", head2);
   }
 
-  console.log("mergedList", mergedList);
+  if (head1 != null) {
+    nodesArr.push(head1.data);
+  }
+  if (head2 != null) {
+    nodesArr.push(head2.data);
+  }
+
+  // console.log("node arr", nodesArr.sort( (a, b) => a - b ) );
+
+  for (let n of nodesArr.sort((a, b) => a - b)) {
+    const node = { data: n, next: null };
+
+    if (mergedList == null) {
+      mergedList = node;
+    } else {
+      let tempList = mergedList;
+
+      while (tempList.next != null) {
+        tempList = tempList.next;
+      }
+
+      tempList.next = node;
+    }
+  }
+
+  return mergedList;
 }
+// console.log(mergeLists mergedList);
 
 console.log(
   mergeLists(
