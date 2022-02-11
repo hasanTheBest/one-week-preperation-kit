fb1db06ed1f94329a96378d286ed0972c73f8090
Mahmud <rainbowmahmud@gmail.com>
2022-02-11 14:50:33 +0600

    Alhamdulillah, superDigit accepted

--- superDigit.js
+++ superDigit.js
@@ -1,16 +1,14 @@
 function superDigit(n, k) {
-  // conver string to array and added
-  let initialSum = 0;
-  for (let d of n.split("")) {
-    initialSum += d;
+  // initial string value summation
+  let sum = 0;
+  for (let d of n) {
+    sum += parseInt(d);
   }
 
-  // multiple the results by k
-  let digits = (initialSum * k).toString();
+  // string value times k
+  let digits = (sum * k).toString();
 
-  // digits = (digits * k).toString();
-
-  // add until get single digit
+  // add the input until reach single digit
   while (digits.length > 1) {
     let sum = 0;
     for (let digit of digits) {
@@ -26,4 +24,4 @@ function superDigit(n, k) {
 }
 
 console.log(superDigit("9875", 4));
-// output: 2
+// output: 8
